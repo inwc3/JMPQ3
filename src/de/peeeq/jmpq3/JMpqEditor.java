35e870a84c89da835f1eda6f12ed039f35c8c6b6
Crigges <andy400@web.de>
2015-11-03 22:12:02 +0100

    removed unused imports

--- src/de/peeeq/jmpq3/JMpqEditor.java
+++ src/de/peeeq/jmpq3/JMpqEditor.java
@@ -1,8 +1,6 @@
 package de.peeeq.jmpq3;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.ByteOrder;
