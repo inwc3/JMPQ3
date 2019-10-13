7c9f0b0f6eadcaf032ebc8aa1add3cc6fc468d98
Frotty <frotty@sunayama.de>
2019-10-13 20:32:00 +0200

    use different zopfli lib

--- src/main/java/systems/crigges/jmpq3/compression/RecompressOptions.java
+++ src/main/java/systems/crigges/jmpq3/compression/RecompressOptions.java
@@ -3,7 +3,7 @@ package systems.crigges.jmpq3.compression;
 public class RecompressOptions {
     public boolean recompress;
     public boolean useZopfli = false;
-    public int iterations = 15;
+    public int iterations = 16;
     public int newSectorSizeShift = 3;
 
     public RecompressOptions(boolean recompress) {
--- src/main/java/systems/crigges/jmpq3/compression/ZopfliHelper.java
+++ src/main/java/systems/crigges/jmpq3/compression/ZopfliHelper.java
@@ -1,24 +1,26 @@
 package systems.crigges.jmpq3.compression;
 
 
-import com.googlecode.pngtastic.core.processing.zopfli.Buffer;
-import com.googlecode.pngtastic.core.processing.zopfli.Options;
-import com.googlecode.pngtastic.core.processing.zopfli.Zopfli;
+import systems.crigges.jmpq3.DebugHelper;
+
+import java.io.ByteArrayOutputStream;
 
 /**
  * Created by Frotty on 09.05.2017.
  */
 public class ZopfliHelper {
-    private final Zopfli compressor;
+    private final ru.eustas.zopfli.Zopfli compressor;
+    private static final byte[] HEADER_MAGIC = new byte[]{120, -38};
 
     public ZopfliHelper() {
-        compressor = new Zopfli(8 * 1024 * 1024);
+        compressor = new ru.eustas.zopfli.Zopfli(8 * 1024 * 1024);
     }
 
     public byte[] deflate(byte[] bytes, int iterations) {
-        Buffer output = compressor.compress(new Options(Options.BlockSplitting.FIRST, iterations), bytes);
-        byte[] outputBytes = new byte[output.getSize()];
-        System.arraycopy(output.getData(), 0, outputBytes, 0, output.getSize());
-        return outputBytes;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        compressor.compress(new ru.eustas.zopfli.Options(ru.eustas.zopfli.Options.OutputFormat.DEFLATE, ru.eustas.zopfli.Options.BlockSplitting.FIRST, iterations),
+            bytes, byteArrayOutputStream);
+
+        return DebugHelper.appendData(HEADER_MAGIC, byteArrayOutputStream.toByteArray());
     }
 }
