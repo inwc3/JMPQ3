ef6782293f5470f788db3b1cb77724dc0168de3b
Frotty <Frotty@users.noreply.github.com>
2021-03-19 22:48:50 +0100

    fix zopfli compression

--- src/main/java/systems/crigges/jmpq3/compression/ZopfliHelper.java
+++ src/main/java/systems/crigges/jmpq3/compression/ZopfliHelper.java
@@ -1,26 +1,32 @@
 package systems.crigges.jmpq3.compression;
 
 
+import ru.eustas.zopfli.Options;
 import systems.crigges.jmpq3.DebugHelper;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 /**
  * Created by Frotty on 09.05.2017.
  */
 public class ZopfliHelper {
     private final ru.eustas.zopfli.Zopfli compressor;
-    private static final byte[] HEADER_MAGIC = new byte[]{120, -38};
 
     public ZopfliHelper() {
-        compressor = new ru.eustas.zopfli.Zopfli(8 * 1024 * 1024);
+        compressor = new ru.eustas.zopfli.Zopfli(4 * 1024 * 1024);
     }
 
     public byte[] deflate(byte[] bytes, int iterations) {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        compressor.compress(new ru.eustas.zopfli.Options(ru.eustas.zopfli.Options.OutputFormat.DEFLATE, ru.eustas.zopfli.Options.BlockSplitting.FIRST, iterations),
-            bytes, byteArrayOutputStream);
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()) {
+            compressor.compress(new ru.eustas.zopfli.Options(Options.OutputFormat.ZLIB,
+                    ru.eustas.zopfli.Options.BlockSplitting.FIRST, iterations),
+                bytes, byteArrayOutputStream);
 
-        return DebugHelper.appendData(HEADER_MAGIC, byteArrayOutputStream.toByteArray());
+            return byteArrayOutputStream.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
